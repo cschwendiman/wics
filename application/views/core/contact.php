c03b028b9fc2375e2357a61a9e68ea27053f7122
Cassie Schwendiman <cassie@cs.utexas.edu>
2011-09-01 14:33:14 -0500

    fill photo and contact pages

---
+++ application/views/core/contact.php
@@ -0,0 +1,36 @@
+<div class="post">
+	<h2>Contact</h2>
+	<p>
+                <table>
+                    <tr>
+                        <td>President</td>
+                        <td><a href="http://www.google.com/recaptcha/mailhide/d?k=01gdJsjDbUOI3oZ5iMUFhnNQ==&amp;c=G1-t990Kv-5ujUdUQt7C_GLypcGWtSQau4USDOG4DV4=" onclick="window.open('http://www.google.com/recaptcha/mailhide/d?k\07501gdJsjDbUOI3oZ5iMUFhnNQ\75\75\46c\75G1-t990Kv-5ujUdUQt7C_GLypcGWtSQau4USDOG4DV4\075', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'); return false;" title="E-mail Joanna">Joanna Smith</a></td>
+                    </tr>
+                    <tr>
+                        <td>Corporate Liaison</td>
+                        <td><a href="http://www.google.com/recaptcha/mailhide/d?k=01_6Le9pWNe3Ny1eqvRYFr3A==&amp;c=eAMbJ8C-cVlmPgMpQngoUeC1jwOxXKRYOfe199SQdko=" onclick="window.open('http://www.google.com/recaptcha/mailhide/d?k\07501_6Le9pWNe3Ny1eqvRYFr3A\75\75\46c\75eAMbJ8C-cVlmPgMpQngoUeC1jwOxXKRYOfe199SQdko\075', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'); return false;" title="E-mail Mariana">Mariana Sada-Coeto</a></td>
+                    </tr>
+                    <tr>
+                        <td>Academic Chair</td>
+                        <td><a href="http://www.google.com/recaptcha/mailhide/d?k=01v3ALP9zktCxvCag6NvPb6Q==&amp;c=RR6oI0Q5kzhxU2jqEI-J0m2FCw8FEn1htlT6-Bww-n0=" onclick="window.open('http://www.google.com/recaptcha/mailhide/d?k\07501v3ALP9zktCxvCag6NvPb6Q\75\75\46c\75RR6oI0Q5kzhxU2jqEI-J0m2FCw8FEn1htlT6-Bww-n0\075', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'); return false;" title="E-mail Simin">Simin Chen</a></td>
+                    </tr>
+                    <tr>
+                        <td>Social Chair</td>
+                        <td><a href="http://www.google.com/recaptcha/mailhide/d?k=01gdJsjDbUOI3oZ5iMUFhnNQ==&amp;c=blTXOUZYMJoY2XArdMFgqnl83Xe2UUJCA_pIOxqyIBs=" onclick="window.open('http://www.google.com/recaptcha/mailhide/d?k\07501gdJsjDbUOI3oZ5iMUFhnNQ\75\75\46c\75blTXOUZYMJoY2XArdMFgqnl83Xe2UUJCA_pIOxqyIBs\075', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'); return false;" title="E-mail Liz">Liz Lass</a></td>
+                    </tr>
+                    <tr>
+                        <td>Volunteering Chair</td>
+                        <td><a href="http://www.google.com/recaptcha/mailhide/d?k=01v3ALP9zktCxvCag6NvPb6Q==&amp;c=-YVkvqHICtsuYbavF55jysb0eDpmqT6bN1crkKxgQ8M=" onclick="window.open('http://www.google.com/recaptcha/mailhide/d?k\07501v3ALP9zktCxvCag6NvPb6Q\75\75\46c\75-YVkvqHICtsuYbavF55jysb0eDpmqT6bN1crkKxgQ8M\075', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'); return false;" title="E-mail Di">Di Huynh</a></td>
+                    </tr>
+                    <tr>
+                        <td>Freshmen Initiatives Coordinator</td>
+                        <td><a href="http://www.google.com/recaptcha/mailhide/d?k=01gdJsjDbUOI3oZ5iMUFhnNQ==&amp;c=X4nND-S_r6i3Gee6Nr6aNr02aOM8nYfEH-p8-63rTVo=" onclick="window.open('http://www.google.com/recaptcha/mailhide/d?k\07501gdJsjDbUOI3oZ5iMUFhnNQ\75\75\46c\75X4nND-S_r6i3Gee6Nr6aNr02aOM8nYfEH-p8-63rTVo\075', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'); return false;" title="E-mail Lisa">Lisa Lippe</a></td>
+                    </tr>
+                    <tr>
+                        <td>Webmaster</td>
+                        <td><a href="http://www.google.com/recaptcha/mailhide/d?k=01gdJsjDbUOI3oZ5iMUFhnNQ==&amp;c=s3cMusUQ9S324uLX4uEUSaCq-bQwwxCbrOGRsvePJUM=" onclick="window.open('http://www.google.com/recaptcha/mailhide/d?k\07501gdJsjDbUOI3oZ5iMUFhnNQ\75\75\46c\75s3cMusUQ9S324uLX4uEUSaCq-bQwwxCbrOGRsvePJUM\075', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'); return false;" title="E-mail Cassie">Cassie Schwendiman</a>
+                        and <a href="http://www.google.com/recaptcha/mailhide/d?k=01gdJsjDbUOI3oZ5iMUFhnNQ==&amp;c=yK2Hxny6D_Mx-Af9JGh0iPluPpRg1-2Hl3LuNO4UZbY=" onclick="window.open('http://www.google.com/recaptcha/mailhide/d?k\07501gdJsjDbUOI3oZ5iMUFhnNQ\75\75\46c\75yK2Hxny6D_Mx-Af9JGh0iPluPpRg1-2Hl3LuNO4UZbY\075', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'); return false;" title="E-mail Rachel">Rachel Gustafson</a></td>
+                    </tr>
+                </table>
+	</p>
+</div>
