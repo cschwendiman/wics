5daedd2252ac93b4ac3ad0ec852e90f09988d24a
Cassie Schwendiman <cassie@cs.utexas.edu>
2011-09-27 22:13:55 -0500

    add other sponsors

--- application/views/core/sponsors.php
+++ application/views/core/sponsors.php
@@ -6,5 +6,10 @@
 <li><a href="http://www.qualcomm.com/" target="_blank"><img src="img/sponsors/qualcomm_logo.jpg"></a></li>
 <li><a href="http://www.lockheedmartin.com/" target="_blank"><img src="img/sponsors/lockheedmartin_logo.gif"></a></li>
 <li><a href="http://www.yahoo.com/" target="_blank"><img src="img/sponsors/yahoo_logo.jpg"></a></li>
+<li><a href="http://www.ibm.com/" target="_blank"><img src="img/sponsors/ibm_logo.gif"></a></li>
+<li><a href="http://www.boeing.com/" target="_blank"><img src="img/sponsors/boeing_logo.jpg"></a></li>
+<li><a href="http://www.salesforce.com/" target="_blank"><img src="img/sponsors/salesforce_logo.jpg"></a></li>
+<li><a href="http://www.accenture.com/" target="_blank"><img src="img/sponsors/accenture_logo.jpg"></a></li>
+<li><a href="http://www.yelp.com/" target="_blank"><img src="img/sponsors/yelp_logo.jpg"></a></li>
 </ul>
 </div>
