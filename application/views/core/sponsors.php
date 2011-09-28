8930e0621123c78a61f2d7d9b304ed4fcb420e9d
Cassie Schwendiman <cassie@cs.utexas.edu>
2011-09-27 21:55:58 -0500

    comment out email signup form and add urls to sponsors page

--- application/views/core/sponsors.php
+++ application/views/core/sponsors.php
@@ -1,10 +1,10 @@
 <div class="post" id="sponsors">
 <h2>Thank you to our sponsors:</h2>
 <ul class="media-grid">
-<li><a href=""><img src="img/sponsors/universitycoop_logo.jpg"></a></li>
-<li><a href=""><img src="img/sponsors/google_logo.png"></a></li>
-<li><a href=""><img src="img/sponsors/qualcomm_logo.jpg"></a></li>
-<li><a href=""><img src="img/sponsors/lockheedmartin_logo.gif"></a></li>
-<li><a href=""><img src="img/sponsors/yahoo_logo.jpg"></a></li>
+<li><a href="http://universitycoop.com/" target="_blank"><img src="img/sponsors/universitycoop_logo.jpg"></a></li>
+<li><a href="http://www.google.com/" target="_blank"><img src="img/sponsors/google_logo.png"></a></li>
+<li><a href="http://www.qualcomm.com/" target="_blank"><img src="img/sponsors/qualcomm_logo.jpg"></a></li>
+<li><a href="http://www.lockheedmartin.com/" target="_blank"><img src="img/sponsors/lockheedmartin_logo.gif"></a></li>
+<li><a href="http://www.yahoo.com/" target="_blank"><img src="img/sponsors/yahoo_logo.jpg"></a></li>
 </ul>
 </div>
